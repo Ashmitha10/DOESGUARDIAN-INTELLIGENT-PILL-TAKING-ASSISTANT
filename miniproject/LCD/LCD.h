2be853f6a6707ec441630e63fa640afe37ecf35c
KORRA ASHMITHA <korraasmitha@gmail.com>
2026-02-24 22:23:31 +0530

    Update LCD.h

--- miniproject/LCD/LCD.h
+++ miniproject/LCD/LCD.h
@@ -8,20 +8,10 @@ void cmdLCD(u8);
 void charLCD(s8);
 //display a string ON LCD function definition
 void strLCD(s8*);
-
 //Display a integer onLCD function definition
-
 void intLCD(s32);
-//Display a float onLCD function definition
-
-void floatLCD(f32,u32);
-//Display a specialcustom characters onLCD function definition
-
-void writetocgRAM(void);
 //Display on LCD function definition
-
 void displayLCD(u8);
-//Display a hexa decimal valueonLCD function definition
 
-void HexLCD(u32);
+
 
